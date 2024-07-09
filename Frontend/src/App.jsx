d368aefd92f6707a0ae1eeb721ea172ecb3ebf27
Shaswath <shaswath.g@kalvium.community>
2024-07-09 12:24:36 +0530

    added routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={Home}></Route>
+          <Route path='/' element={<Home/>}></Route>
         </Routes>
       </BrowserRouter>
     </>
